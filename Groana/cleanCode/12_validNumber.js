89186dd45b655f03b46a9cf6f03862919f5fa0e1
groana <groana7@gmail.com>
2021-03-04 09:57:30 -0500

    refactor: check if a string is a number

--- Groana/cleanCode/12_validNumber.js
+++ Groana/cleanCode/12_validNumber.js
@@ -6,42 +6,15 @@
  "abc" -> false
  */
 
-//  const validNum = (str) => {
-//     let nums = {'.': true, '1': true, '2': true, '3': true, '4': true, '5': true, '6': true, '7': true, '8': true, '9': true, '0': true}
-
-//     for (let i = 0; i < str.length; i++) {
-//       if (!nums[str[i]]) {
-//         return false
-//       }
-//     }
-//     return true
-//   }
-
 const validNum = (str) => {
-  let nums = {
-    '.': true,
-    1: true,
-    2: true,
-    3: true,
-    4: true,
-    5: true,
-    6: true,
-    7: true,
-    8: true,
-    9: true,
-    0: true,
-  };
   let i = 0;
 
   while (i < str.length) {
     if (i === 0 && (str[i] === ' ' || str[i] === '+' || str[i] === '-')) {
       i++;
-    } else if (
-      i === str.length - 1 &&
-      (str[i] === ' ' || str[i] === '+' || str[i] === '-')
-    ) {
+    } else if (i === str.length - 1 && (str[i] === ' ' || str[i] === '+' || str[i] === '-')) {
       i++;
-    } else if (!nums[str[i]]) {
+    } else if (!(str[i] >= 0 || str[i] <= 9 || str[i] === '.') || str[i] === ' ') {
       return false;
     }
     i++;
