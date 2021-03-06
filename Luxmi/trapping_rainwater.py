7539bbceb7d88ae8de7ecc4e59e4bc63f954f975
Luxmi Saha <sphinx20@gmail.com>
2021-03-06 03:46:29 -0800

    New code

--- Luxmi/trapping_rainwater.py
+++ Luxmi/trapping_rainwater.py
@@ -43,18 +43,18 @@ class Solution(object):
             right = height[i]; 
 
             for j in range(i + 1 , n) : 
-                right = max(right, height[j]);
+                right = max(right, height[j])
 
             # Update the maximum water
             res = res + (min(left, right) - height[i]); 
 
-        return res;
+        return res
 
 
 def main():
     sol = Solution()
-    # height = [0,1,0,2,1,0,1,3,2,1,2,1]
-    height = [4,2,0,3,2,5]
+    height = [0,1,0,2,1,0,1,3,2,1,2,1]
+    # height = [4,2,0,3,2,5]
     print(sol.trap(height))
 
 if __name__ == "__main__":
